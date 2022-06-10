385540e0b3b26aba7943ecd0ae2a74f7d59fe803
AGCerdeiras <93091874+AGCerdeiras@users.noreply.github.com>
2022-06-10 08:02:00 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona persona1 = new Persona();
+
+        persona1.setEdad(44);
+        System.out.println(persona1.getEdad());
+
+        persona1.setNombre("Manuel");
+        System.out.println(persona1.getNombre());
+
+        persona1.setTelefono(672153588);
+        System.out.println(persona1.getTelefono());
     }
+
 }
+
+class Persona {
+    private String nombre;
+    private int edad;
+    private int telefono;
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
